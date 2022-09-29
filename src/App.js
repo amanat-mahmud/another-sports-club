7f8d3ad343bf76664303eca5cc4d3c7602740959
AmanatUMahmud <amanatmahmud@gmail.com>
2022-09-29 19:13:47 +0600

    use of useEffect apart from loading data added

--- src/App.js
+++ src/App.js
@@ -18,14 +18,18 @@ function App() {
         <Person time={time}></Person>
         <div className="blog-container">
           <Blog 
-          ques="How does react work?"
-          ans="React allows us to effectively re-construct our DOM in JavaScript and push only those changes to the DOM which have actually occurred.React uses
-          JSX,React Renderer,React Reconciliation,React Keys."
+            ques="How does react work?"
+            ans="React allows us to effectively re-construct our DOM in JavaScript and push only those changes to the DOM which have actually occurred.React uses
+            JSX,React Renderer,React Reconciliation,React Keys."
           ></Blog>
           <Blog
-          ques="Difference between props and state"
-          ans="In props data is passed from one component to another.It is Immutable (cannot be modified).Read only.
-          In state data is passed within the component only.It is Mutable ( can be modified).Can do read and write." 
+            ques="Difference between props and state"
+            ans="In props data is passed from one component to another.It is Immutable (cannot be modified).Read only.
+            In state data is passed within the component only.It is Mutable ( can be modified).Can do read and write." 
+          ></Blog>
+          <Blog
+            ques="use of useEffect apart from loading data"
+            ans="It can be used to get and show data from the local storage to the front-end.By using dependencies useEffect loads from local storage."
           ></Blog>
         </div>
         
