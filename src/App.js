7ad16088f03f9474dd5235f15624799a9a702db3
AmanatUMahmud <amanatmahmud@gmail.com>
2022-09-29 19:07:59 +0600

    Difference between props and state added

--- src/App.js
+++ src/App.js
@@ -17,11 +17,16 @@ function App() {
         <Sports addDuration={addDuration}></Sports>
         <Person time={time}></Person>
         <div className="blog-container">
-        <Blog 
-        ques="How does react work?"
-        ans="React allows us to effectively re-construct our DOM in JavaScript and push only those changes to the DOM which have actually occurred.React uses
-        JSX,React Renderer,React Reconciliation,React Keys."
-        ></Blog>
+          <Blog 
+          ques="How does react work?"
+          ans="React allows us to effectively re-construct our DOM in JavaScript and push only those changes to the DOM which have actually occurred.React uses
+          JSX,React Renderer,React Reconciliation,React Keys."
+          ></Blog>
+          <Blog
+          ques="Difference between props and state"
+          ans="In props data is passed from one component to another.It is Immutable (cannot be modified).Read only.
+          In state data is passed within the component only.It is Mutable ( can be modified).Can do read and write." 
+          ></Blog>
         </div>
         
       </div>
